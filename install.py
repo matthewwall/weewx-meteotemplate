4a7e4e9024b47eb63fe152a8c9b76752e6d40d73
Matthew Wall <mwall@picorino.local>
2016-12-24 00:43:21 -0500

    rc1 version

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def loader():
 class MeteotemplateInstaller(ExtensionInstaller):
     def __init__(self):
         super(MeteotemplateInstaller, self).__init__(
-            version="0.1",
+            version="0.1rc1",
             name='meteotemplate',
             description='Upload weather data to Meteotemplate.',
             author="Matthew Wall",
@@ -18,6 +18,7 @@ class MeteotemplateInstaller(ExtensionInstaller):
             config={
                 'StdRESTful': {
                     'Meteotemplate': {
-                        'host': 'INSERT_HOST_HERE'}}},
+                        'host': 'INSERT_HOST_HERE',
+                        'password': 'INSERT_PASSWORD_HERE'}}},
             files=[('bin/user', ['bin/user/meteotemplate.py'])]
             )
